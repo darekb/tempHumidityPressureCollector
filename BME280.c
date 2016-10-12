0f8717cb1f5b3ab38bf13722ff96d09862d62b7a
darekb <kontakt@3wm.pl>
2016-10-12 13:04:48 +0200

    test

--- BME280.c
+++ BME280.c
@@ -298,7 +298,7 @@ int32_t BME280_CompensateT(int32_t adc_T) {
 // Output value of �24674867� represents 24674867/256 = 96386.2 Pa = 963.862 hPa
 uint32_t BME280_CompensateP(int32_t adc_P) {
   int32_t var1, var2, p;
-
+//test
   //orginal and documentation
 //  var1 = ((int64_t) t_fine) - 128000;
 //  var2 = var1 * var1 * (int64_t) CalibParam.dig_P6;
